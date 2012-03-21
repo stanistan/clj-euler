82d64cb3538e471abc328c5f15bfaec3059f1bbb
Stan Rozenraukh <stan@stanistan.com>
2012-03-21 09:04:34 -0400

    fix spacing

--- problems.clj
+++ problems.clj
@@ -1,40 +1,42 @@
 
 (load-file "utils.clj");
-
+  
 (defn problem1 []
-	(reduce + (filter #(or (zero? (mod % 3))
-												 (zero? (mod % 5))) 
-						(range 1000))))
+  (reduce + (filter #(or (zero? (mod % 3))
+	                       (zero? (mod % 5))) 
+            (range 1000))))
 
 (defn problem2 []
-	(let [func-size #(%1 (count %2))
-				push-thing #(conj %2 (func-size %1 %2))
-				last-or-zero #(if (nil? (last %)) 0 (last %))
-				lazy-fib #(cond (>= (last-or-zero %2) %1) (pop %2)
-												:else (recur %1 (push-thing fib %2)))]
-				(reduce + (filter even? (lazy-fib 4000000 [0])))))
+  (let [func-size #(%1 (count %2))
+        push-thing #(conj %2 (func-size %1 %2))
+        last-or-zero #(if (nil? (last %)) 0 (last %))
+        lazy-fib #(cond (>= (last-or-zero %2) %1) (pop %2)
+                        :else (recur %1 (push-thing fib %2)))]
+  (reduce + (filter even? (lazy-fib 4000000 [0])))))
 
 (defn problem3 []
-	(last (prime-factors 600851475143)))
+  (last (prime-factors 600851475143)))
 
 (defn problem4 [] 
-	(let [r (range 100 1000)]
-		(reduce max (for [x r y r :when (palindrome? (* x y))] (* x y)))))
+  (let [r (range 100 1000)]
+    (reduce max (for [x r y r :when (palindrome? (* x y))] (* x y)))))
 
 (defn problem5 []
-	(let [r (range 2 21)
-				factored (partition-by identity (flatten (map prime-factors r)))
-				grouped (group-by :num (map count-pieces factored))
-				get-multi (fn [n]
-					(let [a (last (sort-by :count n))] 
-						(Math/pow (:num a) (:count a))))]
-		(int (reduce * (map #(get-multi (second %)) grouped)))))
+  (let [r (range 2 21)
+        factored (partition-by identity (flatten (map prime-factors r)))
+        grouped (group-by :num (map count-pieces factored))
+        get-multi (fn [n]
+          (let [a (last (sort-by :count n))] 
+            (Math/pow (:num a) (:count a))))]
+    (int (reduce * (map #(get-multi (second %)) grouped)))))
 
 (defn problem6 []
-	(let [r (range 101)
-				sum-squares (reduce + (map square r))
-				square-sums (square (reduce + r))]
-		(int (- square-sums sum-squares))))
+  (let [r (range 101)
+        sum-squares (reduce + (map square r))
+        square-sums (square (reduce + r))]
+  (int (- square-sums sum-squares))))
+
+(defn problem7 [])
 
 ; (println (str "problem1: " (problem1)))
 ; (println (str "problem2: " (problem2)))
--- utils.clj
+++ utils.clj
@@ -1,20 +1,20 @@
 (defn fib [n]
-	(if (<= n 1) 
-		n
-		(+ (fib (dec n)) (fib (- n 2)))))
+  (if (<= n 1) 
+    n
+    (+ (fib (dec n)) (fib (- n 2)))))
 
 (def fib (memoize fib))
 
 (defn divides? [n d]
-	(zero? (mod d n)))
+  (zero? (mod d n)))
 
 (defn vcontains? [v thing]
-	(not (nil? (some #{thing} v))))
+  (not (nil? (some #{thing} v))))
 
 (defn push-vec [v piece]
-	(if (vcontains? v piece) 
-		v
-		(conj v piece)))
+  (if (vcontains? v piece) 
+    v
+    (conj v piece)))
 
 (def fib-seq 
   ((fn rfib [a b] 
@@ -22,28 +22,28 @@
    0 1))
 
 (defn palindrome? [n]
-	(let [e (reverse (str n))]
-		(= e (reverse e))))
+  (let [e (reverse (str n))]
+    (= e (reverse e))))
 
 (defn push-if-contains [v piece]
-	(if (vcontains? v piece) v (conj v piece)))
+  (if (vcontains? v piece) v (conj v piece)))
 
 (defn prime-factors 
-	([n] (prime-factors n 2 []))
-	([n factor] (prime-factors n factor []))
-	([n factor factors] 
-		(let [r (conj factors factor)]
-			(if (= n factor) r
-				(if (divides? factor n)
-						(prime-factors (/ n factor) factor r)
-						(prime-factors n (inc factor) factors))))))
+  ([n] (prime-factors n 2 []))
+  ([n factor] (prime-factors n factor []))
+  ([n factor factors] 
+    (let [r (conj factors factor)]
+      (if (= n factor) r
+        (if (divides? factor n)
+            (prime-factors (/ n factor) factor r)
+            (prime-factors n (inc factor) factors))))))
 
 (defn count-pieces [arr]
-	{ :num (first arr) 
-		:count (count arr) })
+  { :num (first arr) 
+    :count (count arr) })
 
 (defn pow [b p]
-	(Math/pow b p))
+  (Math/pow b p))
 
 (defn square [n]
-	(pow n 2))
+  (pow n 2))
